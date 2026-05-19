f6f9ada98d35119880ab1fad3943e4751a13ac32
prasanth <durgaprasanth003@gmail.com>
2026-05-19 20:24:42 +0530

    variables

--- 08-arrays.sh
+++ 08-arrays.sh
@@ -5,7 +5,5 @@ names=("varun" "modi" "venkat")
 echo "the first name is ${names[0]}"
 echo "the second name is ${names[3]}"
 echo "the third name is ${names[1]}"
-
 #@ represents all the names it will print all 
-
-echo "all names ${names[@]}
+echo "all names ${names[@]}"
