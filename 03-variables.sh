5bc4a9860f02ad72f0442846ca8876200bb35bd2
prasanth <durgaprasanth003@gmail.com>
2026-05-19 19:45:05 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#this variables to decalre from shell itself 
+person1=1$
+person2=2$
+
+echo "$person1 : hello bro how are you doing ?"
+echo "$person2 : I am doing good, how about you ?"
+echo "$person1 : I am doing good as well, thanks for asking."
+echo "$person2 : So, what are you up to these days ?"
