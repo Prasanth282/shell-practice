37af667336bfebc253730bff9e34837ac6003a7f
prasanth <durgaprasanth003@gmail.com>
2026-05-19 19:37:55 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+person1=trump
+person2=musk
+
+echo "$person1 : hello bro how are you doing ?"
+echo "$person2 : I am doing good, how about you ?"
+echo "$person1 : I am doing good as well, thanks for asking."
+echo "$person2 : So, what are you up to these days ?"
+echo "$person1 : I am busy with my business and politics."
+echo "$person2 : That sounds interesting, I am busy with my space exploration and electric cars"
