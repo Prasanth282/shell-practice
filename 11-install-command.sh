dd4d029c45db22e04495bcd8796efb98e5efd4f1
prasanth <durgaprasanth003@gmail.com>
2026-05-19 22:33:44 +0530

    variables

--- 11-install-command.sh
+++ 11-install-command.sh
@@ -9,16 +9,23 @@ then
     
 else 
 echo "error :: you don't have root user privlages"
+exit 1
+
+ fi 
 
 #checking whether ine application is installed in the server or not 
  
- dnf list installed | grep mysql 
+ dnf list installed mysql 
  
  if [ $? -eq 0 ]
- 
- then
- echo "mysql is already installed in the server"
- else
-  dnf install mysql -y
 
- fi
+ then
+ echo "mysql is not installed in Your server"
+
+ dnf instsall mysql -y
+
+ else
+  echo "mysql is already installed in your server"
+exit 1
+
+fi
