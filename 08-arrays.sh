c0668adcae4cdb575911c59913073ca5e2a3c09f
prasanth <durgaprasanth003@gmail.com>
2026-05-19 20:22:42 +0530

    variables

--- 08-arrays.sh
+++ 08-arrays.sh
@@ -2,9 +2,9 @@
 #array takes all the declared names as starts from 01234 like that 
 names=("varun" "modi" "venkat")
 
-echo "the first name is ${name[0]}"
-echo "the second name is ${name[3]}"
-echio "the third name is ${name[1]}"
+echo "the first name is ${names[0]}"
+echo "the second name is ${names[3]}"
+echo "the third name is ${names[1]}"
 
 #@ represents all the names it will print all 
 
