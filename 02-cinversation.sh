b8103ab9884926f09107289a6dfd052716aff992
prasanth <durgaprasanth003@gmail.com>
2026-05-19 13:47:27 +0530

    hello world created

---
+++ 02-cinversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "trump : Hello, how are you doing ?"
+echo "musk : I am doing good, how about you ?"
+echo "trump : I am doing good as well, thanks for asking."
+echo "musk : So, what are you up to these days ?"
+echo "trump : I am busy with my business and politics."
+echo "musk : That sounds interesting, I am busy with my space exploration and electric cars
+
+
