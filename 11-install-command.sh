88ae5afdbac54a4cebec8e53003ef55fa2480f0b
prasanth <durgaprasanth003@gmail.com>
2026-05-20 11:57:09 +0530

    hello world created

--- 11-install-command.sh
+++ 11-install-command.sh
@@ -8,30 +8,29 @@ then
     echo " you have root user privlages"
 else 
 echo "error :: you don't have root user privlages"
-exit 1
+exit 1 #we can give upto 127 any nbr 
 
 fi 
 
 #checking whether ine application is installed in the server or not 
  
 dnf list installed mysql 
- 
-if [ $? -eq 0 ]
 
-then
- echo "mysql is installed in your server"
-else
- echo "mysql is not installed in your server"
+if [ $? -ne 0 ]
 
+then 
+    echo " mysql is not installed in server"
+    
+    dnf install mysql -y
 
-dnf install mysql -y
+    if [ $? -eq 0 ]
 
-  if [ $? -eq 0 ]
-
-  then
-    echo "mysql is successfully installed in your server"
-    else        
-    echo "failed to install mysql in your server"
-exit 1
-
-fi 
+    then 
+        echo " mysql installation is successful"
+        exit 1
+    else 
+        echo " mysql installation is failed"
+    fi
+else 
+    echo " mysql is already installed in the server nothing to d ..."
+fi
