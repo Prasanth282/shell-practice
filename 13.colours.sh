db5a096c5c03aa653dc370792ddff70d9136ccfd
prasanth <durgaprasanth003@gmail.com>
2026-05-20 21:58:33 +0530

    hello world created

--- 13.colours.sh
+++ 13.colours.sh
@@ -12,7 +12,7 @@ if [ $userid -eq 0 ]
 then 
     echo " you have root user privlages"
 else 
-echo "$R error :: you don't have root user privlages $N"
+echo "-e $R error :: you don't have root user privlages $N"
 exit 1
 fi
 
@@ -33,7 +33,7 @@ dnf list installed httpd
 if [ $? -ne 0 ]
 
 then 
-    echo "$Y httpd is not installed in server $N"
+    echo "-e $Y httpd is not installed in server $N"
     
     dnf install httpd -y
 
@@ -41,7 +41,7 @@ then
     
 
 else
-    echo "$Y httpd is already installed in the server nothing to d ...$N"
+    echo "-e $Y httpd is already installed in the server nothing to d ...$N"
 fi
 
 dnf list installed nginx
@@ -49,14 +49,14 @@ dnf list installed nginx
 if [ $? -ne 0 ]
 
 then 
-    echo "$Y nginx is not installed in server $N"
+    echo "-e $Y nginx is not installed in server $N"
     
     dnf install nginx -y
 
     VALIDATE $? "nginx"
     
 else
-    echo " nginx is already installed in the server nothing to d ..."
+    echo "-e $Y nginx is already installed in the server nothing to d ...$N"
 fi
 
 dnf list installed python3
@@ -64,12 +64,12 @@ dnf list installed python3
 if [ $? -ne 0 ]
 
 then 
-    echo " $R python3 is not installed in server $N"
+    echo "-e $R python3 is not installed in server $N"
     
     dnf install python3 -y
 
     VALIDATE $? "python3"
     
 else
-    echo "$Y python3 is already installed in the server nothing to d ...$N"
+    echo "-e $Y python3 is already installed in the server nothing to d ...$N"
 fi
