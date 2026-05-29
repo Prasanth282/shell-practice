095de668afb5befa5bf58548718c8903a70d4b3f
prasanth <durgaprasanth003@gmail.com>
2026-05-29 08:47:21 +0530

    hello world

--- 21-diskusage.sh
+++ 21-diskusage.sh
@@ -8,11 +8,12 @@ IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 while IFS= read -r line 
 do 
    USAGE=$(echo $line | awk '{print $6}' | cut -d '%' -f1)
-   PARTITION=$(echo $line awk '{print $7}')
+   PARTITION=$(echo "$line" "awk '{print $7}'")
 
    if [ "$USAGE" -ge "$THRESHOLD_LIMIT" ]
    then 
-    MSG+="high disk usage found in $IP : $PARTITION : $USAGE\n"
+    echo " server : $IP "
+    MSG+="high disk usage found in : $PARTITION : $USAGE\n"
    fi           
 
 done <<< "$DISK_USAGE" 
