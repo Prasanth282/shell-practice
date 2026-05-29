3bb289f70b3d63e336c1623997de38763c35bf68
prasanth <durgaprasanth003@gmail.com>
2026-05-29 08:25:12 +0530

    hello world

---
+++ 21-diskusage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Use)
+THRISHOLD_LIMIT=1
+MSG=''
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+while IFS= read -r line 
+do 
+USAGE=(echo $line awk '{print $6}'| cut -d '%' -f1)
+PARTITION=(echo $line awk '{print $7}')
+if [ $USAGE -ge $THRISHOLD ]
+then 
+    MSG+=high disk usage found in $IP : $PARTITION : $USAGE
+fi           
+
+done <<<$DISK_USAGE 
+
+echo -e $MSG
